aab6a24b9956ecd019032ab47eb1018af1b03064
gost1930 <hadbimohamed66@gmail.com>
2025-01-17 00:42:49 +0100

    add OrderStatusPage and content

--- src/pages/OrderStatus/OrderStatus.tsx
+++ src/pages/OrderStatus/OrderStatus.tsx
@@ -1,13 +1,75 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 // components
-import { PagesTitle } from '../../components'
+import { Inputs, PagesTitle } from "../../components";
 
 const OrderStatus = () => {
+  const containerVariants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5, ease: "easeOut" },
+    },
+  };
+
+  const buttonVariants = {
+    hover: {
+      scale: 1.1,
+      backgroundColor: "#444",
+      transition: { duration: 0.3, ease: "easeInOut" },
+    },
+  };
+
   return (
-    <section className='min-h-screen h-fit w-full'>
-        <PagesTitle title="حالة الطلب" />
-    </section>
-  )
-}
+    <motion.section
+      className="min-h-screen h-fit w-full"
+      initial="hidden"
+      animate="visible"
+      variants={containerVariants}
+    >
+      <PagesTitle title="حالة الطلب" />
+      <motion.div
+        className="flex items-center gap-4 my-4"
+        variants={containerVariants}
+      >
+        <Inputs
+          type="text"
+          name="search"
+          id="search"
+          className="bg-transparent border border-gray-300 rounded-2xl px-4 py-2 focus:outline-none focus:border-black"
+          placeholder="تتبع حالة طلبيتك"
+        />
+        <motion.button
+          className="bg-zinc-800 text-white px-4 py-2 rounded-2xl"
+          variants={buttonVariants}
+          whileHover="hover"
+        >
+          بحث
+        </motion.button>
+      </motion.div>
+
+      {/* order status */}
+      <div className="w-full h-full">
+        <h1>هذه الطلبية:</h1>
+
+        <div className="w-full h-fit flex flex-wrap gap-4">
+          <p className="text-zinc-800 bg-gray-300 px-4 py-2 w-fit rounded-2xl ">
+            قيد التجهيز
+          </p>
+          <p className="text-yellow-800 bg-yellow-300/50 px-4 py-2 w-fit rounded-2xl ">
+            قيد التوصيل
+          </p>
+          <p className="text-blue-800 bg-blue-300/50 px-4 py-2 w-fit rounded-2xl ">
+            عند شركة التوصيل
+          </p>
+          <p className="text-green-800 bg-green-300/50 px-4 py-2 w-fit rounded-2xl ">
+            تم التسليم
+          </p>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
 
 export default OrderStatus;
